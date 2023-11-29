30b879799fcfba6fa2adf2e0d0d4ed363055a4f0
HongQuan2003 <148517311+HongQuan2003@users.noreply.github.com>
2023-11-29 10:41:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ input = open('lrc_xray.pkl', 'rb')
 model = pkl.load(input)
 
 st.header('Upload an image')
-image = st.file_uploader('Choose an image', type = (['png', 'jpg', 'jpeg'])
+image = st.file_uploader('Choose an image', type = (['png', 'jpg', 'jpeg']))
 
 if image is not None:
     image = Image.open(image)
